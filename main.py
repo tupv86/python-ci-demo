8af26f6e648156c97359082c595ab1c832f180c4
tupv86 <phamvantu9286@gmail.com>
2025-06-20 09:21:12 +0700

    Khôi phuc ham add để test không lỗi

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def add(a, b):
+    return a + b
+
 @app.route("/")
-def hello():
-    return "Tổng là: " + str(2 + 5)
+def homepage():
+    return f"Tổng là: {add(2, 5)}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)

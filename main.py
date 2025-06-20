43f66e44f686532a28ec55ce8bf3fa4596823112
tupv86 <phamvantu9286@gmail.com>
2025-06-20 09:28:20 +0700

    Sửa Flask app để dùng Port từ biên môi trường

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,4 +11,5 @@ def homepage():
     return f"Tổng là: {add(2, 5)}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
